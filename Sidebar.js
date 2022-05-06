43c13268607ee4352d800afb08bde7f4b8f695d5
Tringapps - ElangovanNagarajan <96721489+Tringapps-ElangovanNagarajan@users.noreply.github.com>
2022-05-06 14:14:19 +0530

    Update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -5,11 +5,11 @@ export default function Sidebar() {
   return (
     <div className="menu">
      
-          <h3 >Home</h3>        
+          <h3>Home</h3>        
           <h3>Main</h3>        
-          <h3 >About Us</h3>        
-          <h3 >Contact Us</h3>
-       
+          <h3>About</h3>        
+          <h3>Contact</h3>
+          <h3>Email</h3>       
     </div>
   );
 }
