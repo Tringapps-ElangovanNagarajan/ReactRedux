cf3bc4023c1e621f5ad610d860f8f0bc0c2b456d
Tringapps - ElangovanNagarajan <96721489+Tringapps-ElangovanNagarajan@users.noreply.github.com>
2022-05-06 14:24:35 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 export default function Header() {
   const name = useSelector((state) => state.user.head);
   return (
-    <div className="top">
+    <div className="head">
       <h1>{name}</h1>
     </div>
   );
