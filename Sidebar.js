589582400f0d469a3e22e672d3a748861c3926e4
Tringapps - ElangovanNagarajan <96721489+Tringapps-ElangovanNagarajan@users.noreply.github.com>
2022-05-06 14:14:34 +0530

    Update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -4,7 +4,6 @@ import React from "react";
 export default function Sidebar() {
   return (
     <div className="menu">
-     
           <h3>Home</h3>        
           <h3>Main</h3>        
           <h3>About</h3>        
